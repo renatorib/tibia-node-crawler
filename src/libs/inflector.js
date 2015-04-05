700ae62ffa014fed1b794392c61440e7812b9295
Renato Ribeiro <renatoribroman@gmail.com>
2015-04-05 09:37:26 -0300

    fix inflector camelCase

--- src/libs/inflector.js
+++ src/libs/inflector.js
@@ -1,14 +1,11 @@
 var inflector = {};
 
 /**
- * Inflector parseKey
- * Transform a bad formated string in a good key name
+ * Inflector camelCase
  */
 
 inflector.camelCase = function(value){
   return value
-    .replace(':', '')
-    .replace(String.fromCharCode(160), ' ')
     .replace(/^([A-Z])|[\s-_](\w)/g, function(match, p1, p2, offset) {
         if (p2) return p2.toUpperCase();
         return p1.toLowerCase();
