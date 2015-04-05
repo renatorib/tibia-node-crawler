a247cdcc487587cb20cf670b96489559db26d7bd
Renato Ribeiro <renatoribroman@gmail.com>
2015-04-05 08:54:24 -0300

    Create Module Manager

---
+++ src/globals/module_manager.js
@@ -0,0 +1,37 @@
+var fs = require('fs');
+
+/**
+ * Module Manager loadParser
+ * It is used to call parser in a requester
+ */
+
+exports.loadParser = function(name){
+  return alias.require('@modules/' + name + '/parser');
+};
+
+/**
+ * Module Manager loadRequester
+ * It is used to call requester in others requesters and to export
+ */
+
+exports.loadRequester = function(name){
+  return alias.require('@modules/' + name + '/requester');
+};
+
+/**
+ * Module Manager loadParser
+ * It is used to exports all requesters in a object
+ */
+
+exports.exportAll = function(){
+  var list = fs.readdirSync('src/modules');
+  var output = {};
+  var self = this;
+  list.forEach(function(name){
+    var moduleContent = fs.statSync('src/modules/' + name);
+    if(moduleContent.isDirectory()){
+      output[name] = self.loadRequester(name);
+    }
+  });
+  return output;
+};
