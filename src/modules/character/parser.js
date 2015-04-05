359104441102e0be4fba17c20a4ab6ea32f73a45
Renato Ribeiro <renatoribroman@gmail.com>
2015-04-05 08:56:00 -0300

    Create character module

---
+++ src/modules/character/parser.js
@@ -0,0 +1,103 @@
+var better = alias.require('@libs/better');
+
+/**
+ * Character Parser
+ */
+
+module.exports = function(body){
+  var CharacterParser;
+
+  CharacterParser = new Parser(body)
+
+    .setData({
+      character: {},
+      achievements: [],
+      deaths: [],
+      account: {},
+      characters: []
+    })
+
+    .setParser(function(){
+      var self = this;
+      var $ = this.$;
+
+      if($('b:contains("Could not find character")').length === 1){
+        this.setError('Could not find character');
+        return;
+      }
+
+      var wrap = function(key) {
+        return $('b:contains("' + key + '")').closest('table');
+      };
+
+      wrap('Character Information')
+        .find('tr:nth-child(n+2)')
+        .each(function() {
+          var td = $(this).find('td');
+          var key = better.key($(td[0]).text());
+          var value = $(td[1]).text().trim();
+
+          switch(key) {
+            case 'achievementPoints':
+            case 'level':
+              value = better.int(value);
+            break;
+          }
+
+          self.data.character[key] = value;
+        });
+
+      wrap('Account Achievements')
+        .find('tr:nth-child(n+2)')
+        .each(function() {
+          var td = $(this).find('td');
+          var achievement = {
+            grade: $(td[0]).length,
+            name: $(td[1]).text().trim(),
+            secret: $(td[2]).find('img').length === 1
+          };
+
+          if(achievement.name !== '')
+            self.data.achievements.push(achievement);
+        });
+
+      wrap('Character Deaths')
+        .find('tr:nth-child(n+2)')
+        .each(function() {
+          var td = $(this).find('td');
+          var death = {
+            date: $(td[0]).text(),
+            level: better.int($(td[1]).text()),
+            by: better.deathBy($(td[1]).text())
+          }
+
+          self.data.deaths.push(death);
+        });
+
+      wrap('Account Information')
+        .find('tr:nth-child(n+2)')
+        .each(function() {
+          var td = $(this).find('td');
+          var key = better.key($(td[0]).text());
+          var value = $(td[1]).text().trim();
+
+          self.data.account[key] = value;
+        });
+
+      wrap('Characters')
+        .find('tr:nth-child(n+3)')
+        .each(function() {
+          var td = $(this).find('td');
+          var character = {
+            name: $(td[3]).find('input[type=hidden]').val(),
+            world: $(td[1]).text(),
+            online: $(td[2]).text() === 'online'
+          };
+
+          self.data.characters.push(character);
+        });
+
+    });
+
+  return CharacterParser;
+}
---
+++ src/modules/character/requester.js
@@ -0,0 +1,18 @@
+var better = alias.require('@libs/better');
+
+/**
+ * Character Requester
+ */
+
+module.exports = function(name, callback){
+  var CharacterRequester;
+
+  CharacterRequester = new Requester()
+    .setName('Character')
+    .setPath('community/')
+    .setData('subtopic', 'characters')
+    .setData('name', better.url(name))
+    .setParser(moduleManager.loadParser('character'))
+    .setCallback(callback)
+    .request();
+}
